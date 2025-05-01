f634324461bef6ae4ff560bd328697d206a20b50
Anthony Alvarez <anthonyalvarez@Mac.lan>
2025-05-01 12:00:22 -0400

    login functionality complete

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,6 +3,14 @@
         <link rel="stylesheet" href="{{ asset('css/landing.css') }}">
     @endpush
     <div class="container container--narrow">
-        <h1>login</h1>
+
+        <div class="card p-4 shadow-lg roundness-lg text-center mt-4">
+            <h1>Admin Login</h1>
+            <form class="d-flex flex-column align-items-center" action="/adminLogin">
+                <input class="mb-3" type="text" name="email" class="login-input" placeholder="Email">
+                <input class="mb-3" type="password" name="password" class="login-input" placeholder="Password">
+                <button class="px-3 text-light text-center" type="submit">Login</button>
+            </form>
+        </div>
     </div>
 </x-main>
